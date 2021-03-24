e2e2f05d98ccb3809e1034d9aff6cb88b676a4d8
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-03-24 18:15:57 -0300

    adiciona exercicios do conteudo

---
+++ exercises/bloco_4/dia_1/conteudo.js
@@ -0,0 +1,77 @@
+/*
+  ! Parte II
+*/
+// const nome = 'Luiz'
+// const birthCity = 'Cascavel'
+// var birthYear = 1994
+
+// console.log(nome, birthCity, birthYear)
+
+// birthYear = 2020
+
+// console.log(birthYear)
+
+// birthCity = "Bsb"
+// // Erro ao tentar alterar valor de uma constante
+
+// console.log(birthCity)
+
+/*
+  ! Parte III
+*/
+
+// let patientId = 50
+// let isEnrolled = true
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com'
+
+// console.log(typeof patientAge)
+
+// patientId = '50'
+
+// console.log(typeof patientId)
+
+// var base = 5
+// var altura = 8
+// var area = base * altura
+// console.log(base, altura, area)
+
+// var perimetro = base * 2 + altura * 2
+// console.log(perimetro)
+
+/*
+  ! Parte IV
+*/
+
+// var nota = 1000
+
+// if (nota >= 80) {
+//   console.log('Parabéns, você foi aprovada(o)!')
+// } else if (nota >= 60) {
+//   console.log('Você está na nossa lista de espera')
+// } else {
+//   console.log('Você foi reprovada(o)')
+// }
+
+/*
+  ! Parte V
+*/
+
+// var status;
+
+// switch(status) {
+//   case 'aprovada':
+//     console.log('Parabéns, você foi aprovada(o)!')
+//     break
+//   case 'lista':
+//     console.log('Você está na nossa lista de espera')
+//     break
+//   case 'reprovada':
+//     console.log('Você foi reprovada(o)')
+//     break
+//   default:
+//     console.log('não se aplica')
+// }
