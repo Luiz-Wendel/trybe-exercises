58919777f4715edd1fb46613521a6dd309202507
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-03-24 18:28:23 -0300

    adiciona ex.02

---
+++ exercises/bloco_4/dia_1/ex-02.js
@@ -0,0 +1,9 @@
+/*
+  Faça um programa que retorne o maior de dois números.
+  Defina no começo do programa duas variáveis com os valores que serão comparados.
+*/
+
+var a = 2;
+var b = 10;
+
+a > b ? console.log(a) : console.log(b);
