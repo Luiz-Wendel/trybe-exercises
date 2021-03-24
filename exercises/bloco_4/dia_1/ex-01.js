96a34991979c673fd95a5093341eb6113e7cbd69
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-03-24 18:25:22 -0300

    adiciona ex.01

---
+++ exercises/bloco_4/dia_1/ex-01.js
@@ -0,0 +1,20 @@
+/* 
+  Faça cinco programas, um para cada operação aritmética básica.
+  Seu programa deve ter duas variáveis, a e b , definidas no começo com os valores que serão operados.
+  Faça programas para:
+
+  Adição
+  Subtração
+  Multiplicação
+  Divisão
+  Módulo
+*/
+
+var a = 2;
+var b = 10;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
