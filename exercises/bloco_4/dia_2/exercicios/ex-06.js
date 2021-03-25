1d1aa48da0d3dda3eb0ad38b3db4bcba8f7f42f7
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-03-25 18:24:34 -0300

    adiciona solucao do ex.06

--- exercises/bloco_4/dia_2/exercicios/ex-06.js
+++ exercises/bloco_4/dia_2/exercicios/ex-06.js
@@ -4,3 +4,9 @@
 */
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+var oddCount = 0;
+
+for (number of numbers) number % 2 !== 0 ? oddCount += 1 : '';
+
+oddCount !== 0 ? console.log(oddCount) : console.log('nenhum valor ímpar encontrado');
