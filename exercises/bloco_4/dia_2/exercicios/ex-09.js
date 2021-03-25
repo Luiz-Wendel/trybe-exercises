444a6d444b5ad8d72f5e11788a88365c9588caaa
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-03-25 18:33:11 -0300

    adiciona solucao do ex.09

--- exercises/bloco_4/dia_2/exercicios/ex-09.js
+++ exercises/bloco_4/dia_2/exercicios/ex-09.js
@@ -1,3 +1,8 @@
 /* Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 */
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+var array = [];
+var result = 0;
+
+for (let i = 1; i <= 25; i += 1) array.push(i);
+
+for (number of array) console.log(number / 2);
