603d38347e666833ae7cadb52ebeae78f6c60c12
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-03-25 18:13:24 -0300

    adiciona solucao do ex.02

--- exercises/bloco_4/dia_2/exercicios/ex-02.js
+++ exercises/bloco_4/dia_2/exercicios/ex-02.js
@@ -1,3 +1,9 @@
 /* Para o segundo exercício, você deve somar todos os valores contidos no array e imprimir o resultado */
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+var sum = 0;
+
+for (number of numbers) sum += number;
+
+console.log(sum);
