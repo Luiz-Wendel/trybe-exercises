041a6b1df2f57d52490d4ef0847a9e6fc5c64d3f
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-03-25 15:19:06 -0300

    adiciona exercicios do conteudo de arrays

---
+++ exercises/bloco_4/dia_2/conteudo/arrays.js
@@ -0,0 +1,12 @@
+/* 1 - Obtenha o valor "Serviços" do array menu */
+/* 2 - Procure o índice do valor "Portfólio" do array menu */
+/* 3 - Adicione o valor "Contato" no final do array menu */
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+let indexOfPortfolio = menu[2];
+
+menu.push('Contato');
+
+console.log(menuServices);
+console.log(indexOfPortfolio);
