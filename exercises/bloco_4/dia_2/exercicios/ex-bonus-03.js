7bd8c24f3068d0153062ccb5e47cdc5b342c93ec
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-03-26 15:33:46 -0300

    concerta padrao de if ternario

--- exercises/bloco_4/dia_2/exercicios/ex-bonus-03.js
+++ exercises/bloco_4/dia_2/exercicios/ex-bonus-03.js
@@ -13,8 +13,9 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let number2 = [];
 
 for (let i = 0; i < numbers.length; i += 1) {
-  if (i !== numbers.length - 1) number2.push(numbers[i] * numbers[i + 1]);
-  else number2.push(numbers[i] * 2);
+  i !== numbers.length - 1
+    ? number2.push(numbers[i] * numbers[i + 1])
+    : number2.push(numbers[i] * 2);
 }
 
 console.log(number2);
