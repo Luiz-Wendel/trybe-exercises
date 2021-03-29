c95cc9af159d30488797155a2060e0329d1d4f26
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-03-29 18:50:27 -0300

    adiciona exercicios de funcoes (ex.04)

---
+++ exercises/bloco_4/dia_4/exercicios/funcoes/exercicio-04.js
@@ -0,0 +1,20 @@
+/*
+  Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+
+  Array de teste: ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']; .
+  Valor esperado no retorno da função: Fernanda .
+*/
+
+var array = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+
+function getBiggestName(array) {
+  var biggestName = array[0];
+
+  for (firstName of array) {
+    firstName.length > biggestName.length ? (biggestName = firstName) : '';
+  }
+
+  return biggestName;
+}
+
+console.log(getBiggestName(array));
