d6b0f84125ac936e935d21a34143f094ff279ca0
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-04-06 16:57:38 -0300

    adiciona ex.02

--- exercises/bloco_5/dia_2/exercicios/script.js
+++ exercises/bloco_5/dia_2/exercicios/script.js
@@ -2,3 +2,8 @@ const body = document.getElementsByTagName('body')[0];
 const h1 = document.createElement('h1');
 h1.innerText = 'Exercício 5.2 - JavaScript DOM';
 body.appendChild(h1);
+
+const div = document.createElement('div');
+const mainContent = div.cloneNode();
+mainContent.className = 'main-content';
+body.appendChild(mainContent);
