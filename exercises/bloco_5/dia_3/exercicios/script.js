62557d6a58d189123dd21068da63955ee5750081
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-04-07 16:52:24 -0300

    adiciona ex.01

--- exercises/bloco_5/dia_3/exercicios/script.js
+++ exercises/bloco_5/dia_3/exercicios/script.js
@@ -21,4 +21,22 @@ function createDaysOfTheWeek() {
 
 createDaysOfTheWeek();
 
-// Escreva seu código abaixo.
+const decDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+const holidays = [24, 25, 31];
+const fridays = [4, 11, 18, 25];
+
+const liDay = document.createElement('li');
+liDay.classList.add('day');
+
+const ulDays = document.querySelector('#days');
+
+for (const day of decDaysList) {
+  const newLi = liDay.cloneNode();
+
+  if (holidays.includes(day)) newLi.classList.add('holiday');
+  if (fridays.includes(day)) newLi.classList.add('friday');
+
+  newLi.innerText = day;
+
+  ulDays.appendChild(newLi);
+}
