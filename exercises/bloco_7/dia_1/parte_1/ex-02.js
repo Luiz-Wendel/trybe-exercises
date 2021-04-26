b11362dce65e024c1df79386d297ccd00b611265
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-04-26 15:48:02 -0300

    adiciona parte 1 - ex.02

--- exercises/bloco_7/dia_1/parte_1/ex-02.js
+++ exercises/bloco_7/dia_1/parte_1/ex-02.js
@@ -1,5 +1,5 @@
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-// Seu código aqui.
+const ascendingOrder = (array) => array.sort((firstNumber, secondNumber) => firstNumber - secondNumber);
 
-console.log(oddsAndEvens);
+console.log(`Os números ${ascendingOrder(oddsAndEvens)} se encontram ordenados de forma crescente!`);
