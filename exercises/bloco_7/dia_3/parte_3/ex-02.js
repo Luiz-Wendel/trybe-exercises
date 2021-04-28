e4bfc6b262e0bc4b96c6ea547590a84758a6876c
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-04-28 19:28:38 -0300

    adiciona parte 3 ex.02

--- exercises/bloco_7/dia_3/parte_3/ex-02.js
+++ exercises/bloco_7/dia_3/parte_3/ex-02.js
@@ -1,6 +1,9 @@
+const assert = require('assert');
+
 const removeVowels = (word) => {
   const characters = word.split('');
   const results = [];
+  let counter = 0;
 
   for (let index = 0; index < characters.length; index += 1) {
     if (
@@ -10,14 +13,16 @@ const removeVowels = (word) => {
       characters[index] === 'e' ||
       characters[index] === 'u'
     ) {
-      results.push(characters[index]);
+      counter += 1;
+      results.push(counter);
     } else {
-      results.push('_');
+      results.push(characters[index]);
     }
   }
-  return results;
+  return results.join('');
 };
-
 
 const parameter = 'Dayane';
 const result = 'D1y2n3';
+
+assert.deepStrictEqual(removeVowels(parameter), result);
