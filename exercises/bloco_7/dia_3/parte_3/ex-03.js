9fa5f9ad338948cb1730557b6e7d28c521956ced
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-04-28 19:33:04 -0300

    adiciona parte 3 ex.03

--- exercises/bloco_7/dia_3/parte_3/ex-03.js
+++ exercises/bloco_7/dia_3/parte_3/ex-03.js
@@ -1,12 +1,8 @@
-const greaterThanTen = (array) => {
-  let results = 0;
-  for (let index = 0; index < array.length; index += 1) {
-    if (array[index] > 10) {
-      results += array[index];
-    }
-  }
-  return results;
-};
+const assert = require('assert');
+
+const greaterThanTen = (array) => array.filter((number) => number > 10);
 
 const parameter = [4, 10, 32, 9, 21];
 const result = [32, 21];
+
+assert.deepStrictEqual(greaterThanTen(parameter), result);
