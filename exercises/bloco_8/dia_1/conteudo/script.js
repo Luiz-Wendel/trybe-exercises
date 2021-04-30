804ba13b849b494cea7e98bd4163bc35ede63044
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-04-30 15:14:54 -0300

    adiciona exercicio do conteudo

--- exercises/bloco_8/dia_1/conteudo/script.js
+++ exercises/bloco_8/dia_1/conteudo/script.js
@@ -4,3 +4,13 @@
   3. Crie mais uma função de primeira classe que tenha o retorno console.log('Partiu dormir!!') ;
   4. Desenvolva uma HOF chamada doingThings e configure esta função para que retorne a execução das funções de primeira classe que você criou nos exemplos anteriores.
 */
+
+const waking = () => console.log('Acordando!!');
+const drinking = () => console.log('Bora tomar café!!');
+const sleeping = () => console.log('Partiu dormir!!');
+
+const doingThings = (thing) => thing();
+
+doingThings(waking);
+doingThings(drinking);
+doingThings(sleeping);
