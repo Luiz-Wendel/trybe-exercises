6c984e1c707554d613da07bebb137fc9f2a347e9
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-05-12 16:53:53 -0300

    adiciona ex.01

--- exercises/bloco_9/dia_02/jokeApiScript.js
+++ exercises/bloco_9/dia_02/jokeApiScript.js
@@ -1,7 +1,21 @@
+// apiScript.js     
 const API_URL = 'https://icanhazdadjoke.com/';
 
 const fetchJoke = () => {
-  // Adicionar lógica aqui!
+  const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' }
+  };
+
+  fetch(API_URL, myObject)
+    .then((response) => response.json())
+    .then(data => {
+      const paragraph = document.createElement('p');
+      paragraph.innerHTML = data.joke;
+      const jokeContainer = document.getElementById('jokeContainer');
+      jokeContainer.appendChild(paragraph);
+    })
+    .catch((error) => console.log(error));
 };
 
 window.onload = () => fetchJoke();
