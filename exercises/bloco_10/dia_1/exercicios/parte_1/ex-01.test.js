b1c53ddc9719fe3509663865e84e7b0714ff3360
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-05-18 18:31:24 -0300

    adiciona parte 1 - ex.01

---
+++ exercises/bloco_10/dia_1/exercicios/parte_1/ex-01.test.js
@@ -0,0 +1,23 @@
+const sum = require('./ex-01.js');
+
+describe('Testing Sum function', () => {
+  it('should be a function', () => {
+    expect(typeof sum).toBe('function');
+  });
+
+  it('should return 9 if 4 and 5 is passed', () => {
+    expect(sum(4, 5)).toBe(9);
+  });
+
+  it('should return 0 if 0 and 0 is passed', () => {
+    expect(sum(0, 0)).toBe(0);
+  });
+
+  it('should throw an error when a string is passed', () => {
+    expect(() => sum(5, "5")).toThrow();
+  });
+
+  it('should throw the message "parameters must be numbers" when a string is passed', () => {
+    expect(() => sum(5, "5")).toThrow(new Error('parameters must be numbers'));
+  });
+});
