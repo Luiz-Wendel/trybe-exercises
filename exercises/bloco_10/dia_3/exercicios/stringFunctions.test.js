64f102888991dcffa3688b853d891f342e52dde4
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-05-21 11:42:26 -0300

    adiciona ex.04

---
+++ exercises/bloco_10/dia_3/exercicios/stringFunctions.test.js
@@ -0,0 +1,44 @@
+const stringFunctions = require('./stringFunctions');
+
+jest.mock('./stringFunctions')
+
+describe('#stringFunctions', () => {
+  describe('uppercase mock', () => {
+    it('should return string in lowercase when mocked #ex.04', () => {
+      stringFunctions.uppercase.mockImplementation((string) => string.toLowerCase());
+
+      stringFunctions.uppercase('XABLAU');
+      expect(stringFunctions.uppercase).toHaveBeenCalled();
+      expect(stringFunctions.uppercase('TEST')).toBe('test');
+      expect(stringFunctions.uppercase('THIS IS A TEST')).toBe('this is a test');
+      expect(stringFunctions.uppercase).toHaveBeenCalledTimes(3);
+    });
+  });
+
+  describe('firstChar mock', () => {
+    it('should return the last char when mocked #ex.04', () => {
+      stringFunctions.firstChar.mockImplementation((string) => string.slice(-1));
+
+      stringFunctions.firstChar('XABLAU');
+      expect(stringFunctions.firstChar).toHaveBeenCalled();
+      expect(stringFunctions.firstChar('testing')).toBe('g');
+      expect(stringFunctions.firstChar('this is a test')).toBe('t');
+      expect(stringFunctions.firstChar('xablau')).toBe('u');
+      expect(stringFunctions.firstChar).toHaveBeenCalledTimes(4);
+    });
+  });
+
+  describe('concat mock', () => {
+    it('should return the concatenation of three strings when mocked #ex.04', () => {
+      stringFunctions.concat.mockImplementation(
+        (firstString, secondString, thirdString) => `${firstString}${secondString}${thirdString}`
+      );
+
+      stringFunctions.concat('THIS', 'IS', 'XABLAU');
+      expect(stringFunctions.concat).toHaveBeenCalled();
+      expect(stringFunctions.concat('testing ', 'this', ' function')).toBe('testing this function');
+      expect(stringFunctions.concat('THIS ', 'IS', ' XABLAU')).toBe('THIS IS XABLAU');
+      expect(stringFunctions.concat).toHaveBeenCalledTimes(3);
+    });
+  });
+});
