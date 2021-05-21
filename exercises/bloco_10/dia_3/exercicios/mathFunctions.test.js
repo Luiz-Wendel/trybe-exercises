df70717ae61e2ecf1f5700e847133b0dfc59de89
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-05-21 11:21:53 -0300

    adiciona ex.03

--- exercises/bloco_10/dia_3/exercicios/mathFunctions.test.js
+++ exercises/bloco_10/dia_3/exercicios/mathFunctions.test.js
@@ -11,14 +11,14 @@ describe('#mathFunctions', () => {
     it('should return 10 when mocked #ex01', () => {
       const mockedRandomInt = jest.spyOn(functions, 'getRandomNumberBetween')
         .mockReturnValue(10);
-  
+
       functions.getRandomNumberBetween();
       expect(mockedRandomInt).toHaveBeenCalled();
       expect(functions.getRandomNumberBetween()).toBe(10);
       expect(mockedRandomInt).toHaveBeenCalledTimes(2);
       mockedRandomInt.mockRestore();
     });
-  
+
     /*
       Com a mesma função do exercício anterior, utilizando o mock, crie uma nova implementação,
       que deve receber dois parâmetros e retornar a divisão do primeiro pelo segundo.
@@ -34,5 +34,30 @@ describe('#mathFunctions', () => {
     expect(mockedRandomInt).toHaveBeenCalledTimes(2);
     mockedRandomInt.mockRestore();
    });
+  });
+
+  /*
+    Ainda com a mesma função do primeiro exercício, utilizando o mock,
+    crie uma nova implementação que receba três parâmetros e retorne sua multiplicação.
+    Após fazer os devidos testes para ela, resete sua implementação e crie uma nova, que receba um parâmetro e retorne seu dobro.
+    Faça os testes necessários.
+  */
+  it('should #ex.03', () => {
+    const mockedRandomInt = jest.spyOn(functions, 'getRandomNumberBetween')
+      .mockImplementation((firstNumber, secondNumber, thirdNumber) => (
+        firstNumber * secondNumber * thirdNumber
+      ));
+
+    expect(functions.getRandomNumberBetween(2, 2, 2)).toBe(8);
+    expect(functions.getRandomNumberBetween(2, 5, 5)).toBe(50);
+    expect(functions.getRandomNumberBetween(5, 8, 3)).toBe(120);
+    expect(mockedRandomInt).toHaveBeenCalledTimes(3);
+    mockedRandomInt.mockReset();
+    mockedRandomInt.mockImplementation((number) => number * 2);
+    expect(functions.getRandomNumberBetween(4)).toBe(8);
+    expect(functions.getRandomNumberBetween(125)).toBe(250);
+    expect(functions.getRandomNumberBetween(23456)).toBe(46912);
+    expect(mockedRandomInt).toHaveBeenCalledTimes(3);
+    mockedRandomInt.mockRestore();
   });
 });
