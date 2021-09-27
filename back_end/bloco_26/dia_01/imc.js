8743dedcfaad8a4c337aaab087ed25e68a3a3015
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-09-27 18:23:59 -0300

    adiciona ex.05

--- back_end/bloco_26/dia_01/imc.js
+++ back_end/bloco_26/dia_01/imc.js
@@ -6,7 +6,16 @@ var userHeight = readlineSync.question('What\'s your height? ');
 const imc = (weight = 80, height = 190) => {
   const heightInMeters = height > 5 ? height / 100 : height;
 
-  return weight / (heightInMeters ** 2);
+  const imcValue = weight / (heightInMeters ** 2);
+
+  console.log(imcValue);
+
+  if (imcValue < 18.5) console.log('Abaixo do peso (magreza)');
+  else if (imcValue < 25) console.log('Peso normal');
+  else if (imcValue < 30) console.log('Acima do peso (sobrepeso)');
+  else if (imcValue < 35) console.log('Obesidade grau I');
+  else if (imcValue < 40) console.log('Obesidade grau II');
+  else console.log('Obesidade graus III e IV');
 };
 
-console.log(imc(userWeight, userHeight));
+imc(userWeight, userHeight);
