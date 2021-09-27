f1440c94077e6392f74ca61414c8e905380760c5
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-09-27 19:44:03 -0300

    adiciona exercicio bonus 01

--- back_end/bloco_26/dia_01/index.js
+++ back_end/bloco_26/dia_01/index.js
@@ -1,14 +1,14 @@
 const readlineSync = require('readline-sync');
 var exec = require('child_process').exec;
 
-const scriptOptions = ['imc', 'velocity', 'draw'];
+const scriptOptions = ['imc', 'velocity', 'draw', 'factorial'];
 const userPick = readlineSync.keyInSelect(scriptOptions, 'Which script?');
 
 if (userPick >= 0) {
   const selectedScript = scriptOptions[userPick];
 
   exec(`npm run ${selectedScript}`, (error, stdout) => {
-    if (error) console.log('Something went wrong!');
+    if (error) console.log('Something went wrong!', error);
 
     console.log(stdout);
   });
