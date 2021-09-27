58edfc76ecb9cf2995b8855cefd30320ea67f98a
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-09-27 18:14:11 -0300

    adiciona ex.03

--- back_end/bloco_26/dia_01/imc.js
+++ back_end/bloco_26/dia_01/imc.js
@@ -1,7 +1,12 @@
+var readlineSync = require('readline-sync');
+ 
+var userWeight = readlineSync.question('What\'s your weight? ');
+var userHeight = readlineSync.question('What\'s your height? ');
+
 const imc = (weight = 80, height = 190) => {
   const heightInMeters = height > 5 ? height / 100 : height;
 
   return weight / (heightInMeters ** 2);
 };
 
-console.log(imc());
+console.log(imc(userWeight, userHeight));
