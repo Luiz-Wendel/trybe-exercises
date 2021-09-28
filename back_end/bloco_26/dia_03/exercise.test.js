3095f511edb5996afd4b5f6c305a722e29428851
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-09-28 20:11:22 -0300

    adiciona ex.04

--- back_end/bloco_26/dia_03/exercise.test.js
+++ back_end/bloco_26/dia_03/exercise.test.js
@@ -4,6 +4,8 @@ const numberType = require('./exercise');
 
 const positive = 'positive';
 const negative = 'negative';
+const xablau = 'xablau';
+const stringParameter = 'Parameters must be strings!';
 
 describe('numberType function', () => {
   it('should be a function', () => {
@@ -29,10 +31,37 @@ describe('numberType function', () => {
   });
 
   it('should throw an Error if parameter is not a number', () => {
-    expect(() => numberType('xablau')).to.throw();
+    expect(() => numberType(xablau)).to.throw();
   });
 
-  it('should throw an Error if parameter is not a number', () => {
-    expect(() => numberType('xablau')).to.throw(Error, 'Parameter must be a number!');
+  it('should throw a message if parameter is not a number', () => {
+    expect(() => numberType(xablau)).to.throw(Error, 'Parameter must be a number!');
+  });
+});
+
+describe('writeToFile function', () => {
+  it('should be a function', () => {
+    expect(writeToFile).to.be.instanceOf(Function);
+  });
+
+  it('should return an error if a parameter is missing', () => {
+    expect(() => writeToFile(xablau)).to.throw();
+    expect(() => writeToFile()).to.throw();
+  });
+
+  it('should return an error if a parameter is not a string', () => {
+    expect(() => writeToFile(xablau, true)).to.throw();
+    expect(() => writeToFile(45, xablau)).to.throw();
+  });
+
+  it('should throw a message if invalid parameters are passed', () => {
+    expect(() => writeToFile(xablau)).to.throw(Error, stringParameter);
+    expect(() => writeToFile()).to.throw(Error, stringParameter);
+    expect(() => writeToFile(xablau, true)).to.throw(Error, stringParameter);
+    expect(() => writeToFile(45, xablau)).to.throw(Error, stringParameter);
+  });
+
+  it('should write content to file', () => {
+    expect(writeToFile('temp.js', xablau)).to.be.equal('ok');
   });
 });
