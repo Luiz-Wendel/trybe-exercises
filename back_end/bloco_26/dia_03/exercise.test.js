3762dbd1de4ea3385088217a2eba64706ad429b8
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-09-28 18:53:59 -0300

    adiciona ex.01

---
+++ back_end/bloco_26/dia_03/exercise.test.js
@@ -0,0 +1,30 @@
+const { expect } = require("chai");
+
+const numberType = require('./exercise');
+
+const positive = 'positive';
+const negative = 'negative';
+
+describe('numberType function', () => {
+  it('should be a function', () => {
+    expect(numberType).to.be.instanceOf(Function);
+  });
+
+  it('should return a string', () => {
+    expect(numberType()).to.be.a('string');
+  });
+
+  it('should return "positive" if number is greater than 0', () => {
+    expect(numberType(4)).to.be.equal(positive);
+    expect(numberType(53)).to.be.equal(positive);
+  });
+
+  it('should return "negative" if number is lesser than 0', () => {
+    expect(numberType(-3)).to.be.equal(negative);
+    expect(numberType(-26)).to.be.equal(negative);
+  });
+
+  it('should return "neutral" if number is 0', () => {
+    expect(numberType(0)).to.be.equal('neutral');
+  });
+});
