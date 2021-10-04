4c9eeb708d4a074f1393398f1f10f714f3b82050
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-10-04 17:06:58 -0300

    add ex.02

--- back_end/bloco_26/dia_04/index.js
+++ back_end/bloco_26/dia_04/index.js
@@ -3,7 +3,15 @@ const colors = require('colors');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/ping', (_req, res) => res.status(200).json({ message: 'pong' }));
+
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+
+  return res.status(200).json({ message: `Hello, ${name}!` });
+});
 
 app.listen(3000, () => {
   console.log(`App running on ${colors.cyan('port 3000')}...`);
