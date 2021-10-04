70caa297c81deeb3f1d985567d068f2d171dbd7b
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-10-04 17:17:52 -0300

    add ex.03

--- back_end/bloco_26/dia_04/index.js
+++ back_end/bloco_26/dia_04/index.js
@@ -13,6 +13,14 @@ app.post('/hello', (req, res) => {
   return res.status(200).json({ message: `Hello, ${name}!` });
 });
 
+app.post('/greetings', (req, res) => {
+  const { name, age } = req.body;
+
+  if (age > 17) return res.status(200).json({ message: `Hello, ${name}!` });
+
+  return res.status(401).json({ message: 'Unauthorized!' });
+});
+
 app.listen(3000, () => {
   console.log(`App running on ${colors.cyan('port 3000')}...`);
 });
