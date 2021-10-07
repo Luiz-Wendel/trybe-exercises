b7590729c7fb1e773e0819d186822064fb6d5989
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-10-07 18:22:31 -0300

    add ex.06

--- back_end/bloco_26/dia_05/index.js
+++ back_end/bloco_26/dia_05/index.js
@@ -4,6 +4,7 @@ const colors = require('colors');
 const userRouter = require('./routes/userRouter');
 const btcRouter = require('./routes/btcRouter');
 const postsRouter = require('./routes/postsRouter');
+const error = require('./middlewares/error');
 
 const app = express();
 
@@ -12,6 +13,9 @@ app.use(express.json());
 app.use('/user', userRouter);
 app.use('/btc', btcRouter);
 app.use('/posts', postsRouter);
+
+app.use('*', (_req, _res, next) => next({ statusCode: 404, message: 'Route not found!' }));
+app.use(error);
 
 app.listen(3000, () => {
   console.log(`App running on ${ colors.cyan('port 3000')}…`);
