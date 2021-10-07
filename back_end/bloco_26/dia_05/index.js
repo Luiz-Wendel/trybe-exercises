d5127415ec2b7f86cdedc1bd18a3d403deef12c4
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-10-07 16:44:44 -0300

    add ex.04

--- back_end/bloco_26/dia_05/index.js
+++ back_end/bloco_26/dia_05/index.js
@@ -3,6 +3,7 @@ const colors = require('colors');
 
 const userRouter = require('./routes/userRouter');
 const btcRouter = require('./routes/btcRouter');
+const postsRouter = require('./routes/postsRouter');
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 
 app.use('/user', userRouter);
 app.use('/btc', btcRouter);
+app.use('/posts', postsRouter);
 
 app.listen(3000, () => {
   console.log(`App running on ${ colors.cyan('port 3000')}…`);
