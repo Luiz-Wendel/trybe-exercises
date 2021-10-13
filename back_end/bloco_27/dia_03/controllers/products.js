72c55b220216e98332b8442e9c5896137cffa109
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-10-13 16:42:13 -0300

    add ex.01 e 03

--- back_end/bloco_27/dia_03/controllers/products.js
+++ back_end/bloco_27/dia_03/controllers/products.js
@@ -3,19 +3,19 @@ const Products = require('../models/Products');
 
 const router = express.Router();
 
-router.get('/list-products', async (req, res, next) => {
+router.get('/', async (_req, res) => {
   const products = await Products.getAll();
 
   res.send(products);
 });
 
-router.get('/get-by-id/:id', async (req, res, next) => {
+router.get('/:id', async (req, res) => {
   const product = await Products.getById(req.params.id);
 
   res.send(product);
 });
 
-router.post('/add-user', async (req, res) => {
+router.post('/', async (req, res) => {
   const { name, brand } = req.body;
 
   const newProduct = await Products.add(name, brand);
@@ -23,13 +23,13 @@ router.post('/add-user', async (req, res) => {
   res.send(newProduct);
 });
 
-router.post('/delete-user/:id', async (req, res) => {
+router.delete('/:id', async (req, res) => {
   const products = await Products.exclude(req.params.id);
 
   res.send(products);
 });
 
-router.post('/update-user/:id', async (req, res) => {
+router.put('/:id', async (req, res) => {
   const { name, brand } = req.body;
 
   const products = await Products.update(req.params.id, name, brand);
