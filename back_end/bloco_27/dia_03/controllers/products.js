3a341fabcccd3d73f5bdc235cfaa32beabc9108a
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-10-13 16:54:19 -0300

    add ex.04

--- back_end/bloco_27/dia_03/controllers/products.js
+++ back_end/bloco_27/dia_03/controllers/products.js
@@ -4,37 +4,57 @@ const Products = require('../models/Products');
 const router = express.Router();
 
 router.get('/', async (_req, res) => {
-  const products = await Products.getAll();
+  try {
+    const products = await Products.getAll();
 
-  res.json(products);
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong...' });
+  }
 });
 
 router.get('/:id', async (req, res) => {
-  const product = await Products.getById(req.params.id);
+  try {
+    const product = await Products.getById(req.params.id);
 
-  res.json(product);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong...' });
+  }
 });
 
 router.post('/', async (req, res) => {
-  const { name, brand } = req.body;
+  try {
+    const { name, brand } = req.body;
 
-  const newProduct = await Products.add(name, brand);
+    const newProduct = await Products.add(name, brand);
 
-  res.json(newProduct);
+    res.status(201).json(newProduct);
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong...' });
+  }
 });
 
 router.delete('/:id', async (req, res) => {
-  const products = await Products.exclude(req.params.id);
+  try {
+    await Products.exclude(req.params.id);
 
-  res.json(products);
+    res.status(204).json();
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong...' });
+  }
 });
 
 router.put('/:id', async (req, res) => {
-  const { name, brand } = req.body;
+  try {
+    const { name, brand } = req.body;
 
-  const products = await Products.update(req.params.id, name, brand);
+    const products = await Products.update(req.params.id, name, brand);
 
-  res.json(products);
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong...' });
+  }
 });
 
 module.exports = router;
