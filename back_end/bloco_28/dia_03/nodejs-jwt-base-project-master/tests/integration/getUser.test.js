1520d32f8ac6bf1a595ad84d9e5b21bd083a4d16
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-16 18:47:22 -0300

    add ex.02 - tests

--- back_end/bloco_28/dia_03/nodejs-jwt-base-project-master/tests/integration/getUser.test.js
+++ back_end/bloco_28/dia_03/nodejs-jwt-base-project-master/tests/integration/getUser.test.js
@@ -4,6 +4,7 @@ const { MongoClient } = require('mongodb');
 const sinon = require('sinon');
 const { getConnection } = require('../connectionMock');
 const server = require('../../api/app');
+const { users } = require('../mockData');
 
 chai.use(chaiHttp);
 
@@ -30,6 +31,41 @@ describe('GET /api/users/:userId', () => {
 
     it('should return status code 400', () => {
       expect(response.status).to.be.equal(400);
+    });
+
+    it('should return a body as an object', () => {
+      expect(response.body).to.be.an('object');
+    });
+
+    it('should return a body with the "message" property', () => {
+      expect(response.body).to.have.ownProperty('message');
+    });
+
+    it(`should return the error message: "${errorMessage}"`, () => {
+      expect(response.body.message).to.be.equal(errorMessage);
+    });
+  });
+
+  describe('when the param is not equal the ID of the authenticated user', () => {
+    const errorMessage = 'Access denied';
+    const user = users[0];
+
+    before(async () => {
+      const usersCollection = connectionMock.db('jwt_exercise').collection('users');
+      await usersCollection.insertOne({ ...user });
+
+      const { token } = await chai.request(server)
+        .post('/api/login')
+        .send({ ...user })
+        .then((response) => response.body);
+
+      response = await chai.request(server)
+        .get('/api/users/test')
+        .set('authorization', token);
+    });
+
+    it('should return status code 401', () => {
+      expect(response.status).to.be.equal(401);
     });
 
     it('should return a body as an object', () => {
---
+++ back_end/bloco_28/dia_03/nodejs-jwt-base-project-master/tests/mockData.js
@@ -0,0 +1,12 @@
+module.exports = {
+  users: [
+    {
+      username: 'test',
+      password: '123456',
+    },
+    {
+      username: 'user',
+      password: '123456',
+    }
+  ],
+};
