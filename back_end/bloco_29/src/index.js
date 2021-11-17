0dd8201a0a527af8d97524766553957a1debb943
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-17 18:47:45 -0300

    add ex.05

--- back_end/bloco_29/src/index.js
+++ back_end/bloco_29/src/index.js
@@ -13,6 +13,14 @@ app.get('/', (_req, res) => {
   return res.status(200).json({ environment: NODE_ENV });
 });
 
+app.get('/prod-error', (_req, res) => {
+  const { NODE_ENV } = process.env;
+
+  if (NODE_ENV === 'production') process.exit(500);
+
+  return res.status(200).json({ message: 'ok' });
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`App running on port: ${colors.cyan(PORT)}`));
