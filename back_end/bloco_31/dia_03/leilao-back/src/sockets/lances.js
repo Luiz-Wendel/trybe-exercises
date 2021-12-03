1fefee3595e92725dd51e9c0695f3cdc82b9b769
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-12-03 17:36:59 -0300

    feat: add lances socket

---
+++ back_end/bloco_31/dia_03/leilao-back/src/sockets/lances.js
@@ -0,0 +1,10 @@
+module.exports = (io) => {
+  io.on('connection', (socket) => {
+    socket.on('lance', ({ name, value }) => {
+      console.log(`Lance feito no produto: ${ name }`);
+
+      const newValue = value + 5;
+      io.emit('atualizaValor', newValue);
+    });
+  });
+};
