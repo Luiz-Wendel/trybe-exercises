155ebdee4bc281f76ff1ed0fd70aed611b8ab534
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-12-03 17:02:46 -0300

    feat: add socket connection

--- back_end/bloco_31/dia_03/leilao-back/src/index.js
+++ back_end/bloco_31/dia_03/leilao-back/src/index.js
@@ -7,4 +7,15 @@ const http = require('http').createServer(app);
 
 const { PORT = 3000 } = process.env;
 
+const io = require('socket.io')(http, {
+  cors: {
+    origin: `http://localhost:${PORT}`,
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`Novo usuário conectado. ID: ${socket.id}`);
+});
+
 http.listen(PORT, () => console.log(`Servidor rodando na porta: ${colors.cyan(PORT)}`));
