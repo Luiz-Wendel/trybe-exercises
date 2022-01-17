0e23c15157c5a1d13d8cb8020eba3e4681bd68ca
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2022-01-17 19:49:57 -0300

    feat: add ex.01

---
+++ computer_science/bloco_32/dia_01/ex01.py
@@ -0,0 +1,8 @@
+def getMax(firstNumber, secondNumber):
+    return max(firstNumber, secondNumber)
+
+
+if __name__ == "__main__":
+    print(getMax(2, 5))
+    print(getMax(3, 120))
+    print(getMax(223, 52))
