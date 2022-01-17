b8b43ff218468e1a84044f6da8b9d4440e661d8c
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2022-01-17 19:55:20 -0300

    feat: add ex.02

---
+++ computer_science/bloco_32/dia_01/ex02.py
@@ -0,0 +1,8 @@
+def arithmeticAverage(numbers):
+    return sum(numbers) / len(numbers)
+
+
+if __name__ == "__main__":
+    print(arithmeticAverage([1, 2, 3, 4, 5]))
+    print(arithmeticAverage([12, 21]))
+    print(arithmeticAverage([10, 20, 30, 40, 50]))
