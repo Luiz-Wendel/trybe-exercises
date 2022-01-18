f643a96ba074e641dc8ce4512646050dcf507374
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2022-01-18 00:38:02 -0300

    feat: add bonus 02

---
+++ computer_science/bloco_32/dia_01/bonus02.py
@@ -0,0 +1,9 @@
+def printTriangle(base):
+    for index in range(base + 1):
+        print("*" * index)
+
+
+if __name__ == "__main__":
+    printTriangle(3)
+    printTriangle(5)
+    printTriangle(8)
