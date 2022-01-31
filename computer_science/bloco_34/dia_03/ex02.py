1bbc322a894c965c0159bfbaea328f338a62884d
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2022-01-31 19:29:32 -0300

    add ex.02 (solution)

--- computer_science/bloco_34/dia_03/ex02.py
+++ computer_science/bloco_34/dia_03/ex02.py
@@ -1,22 +1,51 @@
-class Carta:
-    def __init__(self, valor, naipe):
-        self.valor = valor
-        self.naipe = naipe
+class CardDeckIterable:
+    def __init__(self, cards):
+        self.cards = cards
+
+    def __iter__(self):
+        return CardDeckIterator(self.cards)
+
+
+class CardDeckIterator:
+    def __init__(self, cards):
+        self.cards = cards
+        self.current_card = 0
+
+    def __next__(self):
+        try:
+            card = self.cards[self.current_card]
+
+            self.current_card += 1
+            return card
+        except IndexError:
+            raise StopIteration
+
+
+class Card:
+    def __init__(self, value, suite):
+        self.value = value
+        self.suite = suite
 
     def __repr__(self):
-        return "<%s de %s>" % (self.valor, self.naipe)
+        return "<%s of %s>" % (self.value, self.suite)
 
 
-class Baralho:
-    naipes = "copas ouros espadas paus".split()
-    valores = "A 2 3 4 5 6 7 8 9 10 J Q K".split()
+class CardDeck:
+    suite = "hearts diamonds spades clubs".split()
+    values = "A 2 3 4 5 6 7 8 9 10 J Q K".split()
 
     def __init__(self):
-        self._cartas = [
-            Carta(valor, naipe)
-            for naipe in self.naipes
-            for valor in self.valores
+        self._cards = [
+            Card(value, suite) for suite in self.suite for value in self.values
         ]
 
     def __len__(self):
-        return len(self._cartas)
+        return len(self._cards)
+
+    def get_cards(self):
+        return self._cards
+
+
+if __name__ == "__main__":
+    for card in CardDeckIterable(CardDeck().get_cards()):
+        print(card)
