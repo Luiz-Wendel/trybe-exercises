b46d40cebbd829d359fbc35b5ef6055de41c9366
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2022-02-04 17:09:29 -0300

    add ex.03

---
+++ computer_science/bloco_34/dia_03/ex03.py
@@ -0,0 +1,58 @@
+class InverseCardDeckIterable:
+    def __init__(self, cards):
+        self.cards = cards
+
+    def __iter__(self):
+        return InverseCardDeckIterator(self.cards)
+
+
+class InverseCardDeckIterator:
+    def __init__(self, cards):
+        self.cards = cards
+        self.current_card = 0
+
+    def __next__(self):
+        card_length = len(self.cards) - 1
+
+        try:
+            index = card_length - self.current_card
+
+            if index < 0:
+                raise IndexError
+
+            card = self.cards[index]
+
+            self.current_card += 1
+            return card
+        except IndexError:
+            raise StopIteration
+
+
+class Card:
+    def __init__(self, value, suite):
+        self.value = value
+        self.suite = suite
+
+    def __repr__(self):
+        return "<%s of %s>" % (self.value, self.suite)
+
+
+class CardDeck:
+    suite = "hearts diamonds spades clubs".split()
+    values = "A 2 3 4 5 6 7 8 9 10 J Q K".split()
+
+    def __init__(self):
+        self._cards = [
+            Card(value, suite) for suite in self.suite for value in self.values
+        ]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def get_cards(self):
+        return self._cards
+
+
+if __name__ == "__main__":
+    for card in InverseCardDeckIterable(CardDeck().get_cards()):
+        print(card)
