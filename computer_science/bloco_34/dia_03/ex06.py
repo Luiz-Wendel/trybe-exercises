807329331c38a08e2e1550f16ca20b8c79f9e320
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2022-02-04 19:09:47 -0300

    add ex.06 (solution)

--- computer_science/bloco_34/dia_03/ex06.py
+++ computer_science/bloco_34/dia_03/ex06.py
@@ -1,20 +1,43 @@
+from abc import ABC, abstractmethod
+
+
+class CalcularStrategy(ABC):
+    @abstractmethod
+    def calcular(valor):
+        raise NotImplementedError
+
+
+class Iss(CalcularStrategy):
+    def calcular(valor):
+        return valor * 0.1
+
+
+class Icms(CalcularStrategy):
+    def calcular(valor):
+        return valor * 0.06
+
+
+class Pis(CalcularStrategy):
+    def calcular(valor):
+        return valor * 0.0065
+
+
+class Cofins(CalcularStrategy):
+    def calcular(valor):
+        return valor * 0.03
+
+
 class Orcamento:
     def __init__(self, valor):
         self.valor = valor
 
     def calcular_imposto(self, imposto):
-        if imposto == "ISS":
-            return self.__calcular_iss()
-        elif imposto == "ICMS":
-            return self.__calcular_icms()
-
-    def __calcular_iss(self):
-        return self.valor * 0.1
-
-    def __calcular_icms(self):
-        return self.valor * 0.06
+        return imposto.calcular(self.valor)
 
 
-orcamento = Orcamento(1000)
-print(f"ISS: {orcamento.calcular_imposto('ISS')}")
-print(f"ICMS: {orcamento.calcular_imposto('ICMS')}")
+if __name__ == "__main__":
+    orcamento = Orcamento(1000)
+    print(f"ISS: {orcamento.calcular_imposto(Iss)}")
+    print(f"ICMS: {orcamento.calcular_imposto(Icms)}")
+    print(f"ICMS: {orcamento.calcular_imposto(Pis)}")
+    print(f"ICMS: {orcamento.calcular_imposto(Cofins)}")
